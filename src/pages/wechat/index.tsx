3e2f1bd8af4cc1d6c21e9bd5556e538a954a8648
楷鹏:) <wkpcoder@163.com>
2024-03-20 19:45:44 +0800

    Add @boombox/storage and @docusaurus/tsconfig dependencies

--- src/pages/wechat/index.tsx
+++ src/pages/wechat/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageHeader from '@site/src/components/HomepageHeader';
 import WeChat from '@site/src/components/WeChat';
 import Layout from '@theme/Layout';
 
@@ -10,9 +9,8 @@ export default function () {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description='Description will go into a meta tag in <head />'
+      description='You can contact me via WeChat.'
     >
-      <HomepageHeader />
       <main>
         <WeChat />
       </main>
