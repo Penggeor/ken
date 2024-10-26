b61846e5da48deba1cdb88939b07293e5d4bc3e2
wukaipeng <wkpcoder@163.com>
2024-10-26 14:31:07 +0800

    chore: Update description in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -424,7 +424,7 @@ const config: Config = {
           type: 'all',
           title: '生活周刊',
           copyright: `版权 © ${new Date().getFullYear()} 吴楷鹏`,
-          description: `每周更新的生活周刊 #feedId:${process.env.FEED_ID}+userId:${process.env.USER_ID}#`,
+          description: `每周更新的生活周刊 feedId:60244255202015232+userId:69676769296017408`,
         },
         ...MathEquations,
       },
