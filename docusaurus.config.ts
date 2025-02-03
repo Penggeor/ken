aa3472fab582ec0086dcfc5ab5d65754e9076397
wukaipeng <wkpcoder@163.com>
2025-02-03 18:55:43 +0800

    chore: Remove async attribute from external script loading

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -75,7 +75,6 @@ const config: Config = {
       integrity:
         'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
       crossorigin: 'anonymous',
-      async: true,
     },
   ],
 
